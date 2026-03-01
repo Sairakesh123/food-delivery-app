1db738c1c3a1eaf7254f4a1f3d3623c69335fda3
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-01 14:07:26 +0530

    Refactor authService.js for cleaner async functions
    Refactor registerUser and login functions to remove try-catch blocks for cleaner code.

--- Frontend/foodies/src/service/authService.js
+++ Frontend/foodies/src/service/authService.js
@@ -3,20 +3,10 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const API_URL = `${BASE_URL}/api`;
 
-export const registerUser = async(data) => {
-    try {
-        const response = await axios.post(API_URL+"/register", data);
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+export const registerUser = async (data) => {
+    return await axios.post(`${API_URL}/register`, data);
+};
 
 export const login = async (data) => {
-    try {
-        const response = await axios.post(API_URL+"/login", data);
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+    return await axios.post(`${API_URL}/login`, data);
+};
