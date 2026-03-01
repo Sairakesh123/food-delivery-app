b45c59993eaf8760d10089605783ee3574de6142
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-01 14:06:10 +0530

    Refactor API URL to use environment variable

--- Frontend/foodies/src/service/foodService.js
+++ Frontend/foodies/src/service/foodService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/api/foods';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_URL = `${BASE_URL}/api`;
 
 export const fetchFoodList = async() => {
      try {
