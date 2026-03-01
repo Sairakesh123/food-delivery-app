4c56545250eaed46af5e5ee4f9b6e19972e27695
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-01 19:30:45 +0530

    Update API base URL environment variable

--- Frontend/foodies/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/foodies/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -8,7 +8,7 @@ import { toast } from "react-toastify";
 import { RAZORPAY_KEY } from "../../util/constants";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const PlaceOrder = () => {
   const { foodList, quantites, setQuantites, token } =
