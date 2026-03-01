3ac1fa9dfea3b6b5a297610770b247396a38c2df
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-01 14:05:08 +0530

    Update API_URL to use environment variable

--- Frontend/foodies/src/service/authService.js
+++ Frontend/foodies/src/service/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_URL = `${BASE_URL}/api`;
 
 export const registerUser = async(data) => {
     try {
