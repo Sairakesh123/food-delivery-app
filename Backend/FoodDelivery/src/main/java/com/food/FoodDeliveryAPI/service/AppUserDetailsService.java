4811014710092b0e6b790cc50aded4f8593e6e44
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-01 15:38:04 +0530

    Enhance loadUserByUsername to include user roles
    Refactor AppUserDetailsService to include user roles in authorities.

--- Backend/FoodDelivery/src/main/java/com/food/FoodDeliveryAPI/service/AppUserDetailsService.java
+++ Backend/FoodDelivery/src/main/java/com/food/FoodDeliveryAPI/service/AppUserDetailsService.java
@@ -2,6 +2,7 @@ package com.food.FoodDeliveryAPI.service;
 
 import java.util.Collections;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,19 +13,26 @@ import com.food.FoodDeliveryAPI.entity.UserEntity;
 import com.food.FoodDeliveryAPI.repository.UserRepository;
 
 @Service
-public class AppUserDetailsService implements UserDetailsService{
-	private final UserRepository userRepository;
+public class AppUserDetailsService implements UserDetailsService {
 
-	public AppUserDetailsService(UserRepository userRepository) {
-		super();
-		this.userRepository = userRepository;
-	}
+    private final UserRepository userRepository;
 
-	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		UserEntity user = userRepository.findByEmail(email)
-				.orElseThrow(()-> new UsernameNotFoundException("User not found"));
-		return new User(user.getEmail(),user.getPassword(),Collections.emptyList());
-	}
-	
+    public AppUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(
+                        new SimpleGrantedAuthority("ROLE_" + user.getRole())
+                )
+        );
+    }
 }
