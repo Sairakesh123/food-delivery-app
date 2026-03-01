c52e88dbfad706b43d54a4423a7a0db018291007
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-01 11:48:08 +0530

    Remove UserService constructor in UserController
    Removed constructor for UserService injection.

--- Backend/FoodDelivery/src/main/java/com/food/FoodDeliveryAPI/controller/UserController.java
+++ Backend/FoodDelivery/src/main/java/com/food/FoodDeliveryAPI/controller/UserController.java
@@ -20,11 +20,6 @@ public class UserController {
 	
 
     private final UserService userService;
- 
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 	
 	@PostMapping("/register")
 	@ResponseStatus(HttpStatus.CREATED)
