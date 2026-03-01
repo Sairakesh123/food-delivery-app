30a3659aa62961965ce156755ce828a7031ff88c
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-01 15:25:39 +0530

    Update API_URL to use environment variable

--- Frontend/adminpanel/src/services/foodService.js
+++ Frontend/adminpanel/src/services/foodService.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8080/api/foods'
+const API_URL = `${import.meta.env.VITE_API_URL}/foods`;
+
 export const addFood = async (foodData, image) => {
     const formData = new FormData();
     formData.append('food', JSON.stringify(foodData));
     formData.append('file', image);
 
     try {
-        await axios.post(API_URL, formData, { headers: { "Content-Type": "multipart/form-data" } })
+        await axios.post(API_URL, formData);
     } catch (error) {
         console.log('Error', error);
         throw error;
@@ -26,8 +27,8 @@ export const getFoodList = async () => {
 
 export const deleteFood = async (foodId) => {
     try {
-    const response = await axios.delete(API_URL +"/"+foodId);
-    return response.status === 204;
+        const response = await axios.delete(`${API_URL}/${foodId}`);
+        return response.status === 204;
     } catch (error) {
         console.log('Error while deleting the food.',error);
         throw error;
