490c764ba2c9cd0ba8672446207f97c3fe6cf7ff
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-01 14:08:29 +0530

    Update foodService to use new API endpoint

--- Frontend/foodies/src/service/foodService.js
+++ Frontend/foodies/src/service/foodService.js
@@ -1,25 +1,14 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const API_URL = `${BASE_URL}/api`;
+const API_URL = `${BASE_URL}/api/foods`;
 
-export const fetchFoodList = async() => {
-     try {
-      const response =  await axios.get(API_URL);
-      return response.data;
-     } catch (error) {
-        console.log('Error fetching the food list:',error);
-        throw error;
-     }
-   }
+export const fetchFoodList = async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+};
 
-   export const fetchFoodDetails = async (id) => {
-      try {
-         const response = await axios.get(API_URL+"/"+id);
-         return response.data;
-      } catch (error) {
-         console.log('Error fetching food details:' , error);
-         throw error;
-         
-      }
-     }
+export const fetchFoodDetails = async (id) => {
+    const response = await axios.get(`${API_URL}/${id}`);
+    return response.data;
+};
