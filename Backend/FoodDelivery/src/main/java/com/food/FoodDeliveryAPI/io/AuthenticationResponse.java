d98c1595fb3e2f8971bad7b59b3f860f504674e6
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-01 11:46:03 +0530

    Refactor AuthenticationResponse by removing unused methods
    Removed getter methods and constructor from AuthenticationResponse class.

--- Backend/FoodDelivery/src/main/java/com/food/FoodDeliveryAPI/io/AuthenticationResponse.java
+++ Backend/FoodDelivery/src/main/java/com/food/FoodDeliveryAPI/io/AuthenticationResponse.java
@@ -8,18 +8,4 @@ import lombok.Getter;
 public class AuthenticationResponse {
 	private String email;
 	private String token;
-	public String getEmail() {
-		return email;
-	}
-	public String getToken() {
-		return token;
-	}
-	public AuthenticationResponse(String email, String token) {
-		super();
-		this.email = email;
-		this.token = token;
-	}
-	
-	
-	
 }
