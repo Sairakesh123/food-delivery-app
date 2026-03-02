2f07d27cf789958e3759acfee9e44d298d7fca20
Rakesh <rakeshnarayana2002@gmail.com>
2026-03-02 09:02:14 +0530

    Update Login.jsx

--- Frontend/adminpanel/src/pages/Login/Login.jsx
+++ Frontend/adminpanel/src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./login.css";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 export default function Login() {
 
